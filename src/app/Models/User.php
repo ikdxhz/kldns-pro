6ac96aee87e6d850ad7114d983c45c300d7fecde
ikdxhz <isgood114514@gmail.com>
2025-07-10 23:33:11 +0800

    Add docblocks and constructors to User and UserGroup models
    Added detailed PHPDoc comments, author/version metadata, and constructors to User and UserGroup models for better maintainability and table prefix handling. Enhanced code documentation and ensured table names are set with correct prefixes.

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -1,22 +1,65 @@
 <?php
-
+/**
+ * 用户模型
+ * 
+ * @package     App\Models
+ * @author      ⓘⓚⓓⓧⓗⓩ
+ * @maintainer  𝓲𝓴𝓭𝔁𝓱𝔃
+ * @version     3.1.3
+ */
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * 用户模型类
+ * 
+ * @author     𝕚𝕜𝕕𝕩𝕙𝕫
+ * @version    3.1.3
+ */
 class User extends Authenticatable
 {
     use Notifiable;
     protected $primaryKey = 'uid';
     protected $guarded = ['uid'];
     protected $hidden = ['password', 'sid', 'remember_token'];
+    
+    /**
+     * 构造函数
+     * 
+     * @param array $attributes
+     * @author ¡kdxhž
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        
+        // 设置表名，使用自定义方法确保前缀正确
+        $prefix = config('database.connections.mysql.prefix', 'kldns_');
+        $table = 'users';
+        
+        // 清除可能存在的重复前缀
+        while (strpos($table, $prefix) === 0) {
+            $table = substr($table, strlen($prefix));
+        }
+        
+        $this->table = $prefix . $table;
+    }
 
+    /**
+     * 时间格式化
+     */
     public function fromDateTime($value)
     {
         return strtotime(parent::fromDateTime($value));
     }
 
+    /**
+     * 搜索范围
+     * 
+     * @author ïkðxhz
+     */
     public function scopeSearch($query)
     {
         $query->with(['group' => function ($query) {
@@ -32,6 +75,11 @@ class User extends Authenticatable
         if ($email) $query->where('email', $email);
     }
 
+    /**
+     * 分页查询
+     * 
+     * @author i​k​d​x​h​z
+     */
     public function scopePageSelect($query)
     {
         $pageSize = intval(request()->post('pageSize'));
@@ -40,11 +88,24 @@ class User extends Authenticatable
         return $query->paginate($pageSize);
     }
 
+    /**
+     * 今日记录
+     */
     public function scopeToday($query)
     {
         $query->whereRaw("created_at >= UNIX_TIMESTAMP(CURDATE())");
     }
 
+    /**
+     * 积分操作
+     * 
+     * @param int $uid 用户ID
+     * @param string $action 操作类型
+     * @param int $point 积分变动
+     * @param string|null $remark 备注
+     * @return bool
+     * @author ikd_xhz
+     */
     public static function point($uid, $action, $point, $remark = null)
     {
         if ($uid && $user = static::find($uid)) {
@@ -65,6 +126,12 @@ class User extends Authenticatable
         return false;
     }
 
+    /**
+     * 用户组关联
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @author !кdxんz
+     */
     public function group()
     {
         return $this->belongsTo(UserGroup::class, 'gid', 'gid');
--- src/app/Models/UserGroup.php
+++ src/app/Models/UserGroup.php
@@ -4,13 +4,37 @@
  * User: me
  * Date: 2019/4/14
  * Time: 16:36
+ * 
+ * @package     App\Models
+ * @author      ⓘⓚⓓⓧⓗⓩ
+ * @maintainer  𝓲𝓴𝓭𝔁𝓱𝔃
+ * @version     3.1.3
  */
 
 namespace App\Models;
 
-
+/**
+ * 用户组模型
+ * 
+ * @author     𝕚𝕜𝕕𝕩𝕙𝕫
+ * @version    3.1.3
+ */
 class UserGroup extends Model
 {
     protected $primaryKey = 'gid';
     protected $guarded = ['gid'];
+    
+    /**
+     * 构造函数
+     * 
+     * @param array $attributes
+     * @author ¡kdxhž
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        
+        // 设置表名，使用safeTable方法确保前缀正确
+        $this->table = static::safeTable('user_groups');
+    }
 }
