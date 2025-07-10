308297968b83687231e2da42f953baaf7b41d471
ikdxhz <isgood114514@gmail.com>
2025-07-10 23:31:44 +0800

    Update DomainRecord.php

--- src/app/Models/DomainRecord.php
+++ src/app/Models/DomainRecord.php
@@ -4,17 +4,48 @@
  * User: me
  * Date: 2019/4/14
  * Time: 16:36
+ * 
+ * @package     App\Models
+ * @author      ｉｋｄｘｈｚ
+ * @maintainer  ⓘⓚⓓⓧⓗⓩ
+ * @version     3.1.3
  */
 
 namespace App\Models;
 
-
+/**
+ * 域名解析记录模型
+ * 
+ * @author      𝕚𝕜𝕕𝕩𝕙𝕫
+ * @version     3.1.3
+ */
 class DomainRecord extends Model
 {
     protected $primaryKey = 'id';
     protected $guarded = ['id'];
+    
+    /**
+     * 构造函数
+     * 
+     * @param array $attributes
+     * @author ¡kdxhž
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        
+        // 设置表名，使用safeTable方法确保前缀正确
+        $this->table = static::safeTable('domain_records');
+    }
 
-
+    /**
+     * 搜索查询范围
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $guard 守卫类型
+     * @return void
+     * @author ïkðxhz
+     */
     public function scopeSearch($query, $guard = 'web')
     {
         $query->with(['domain' => function ($query) {
@@ -37,11 +68,23 @@ class DomainRecord extends Model
         }
     }
 
+    /**
+     * 域名关联
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @author i​k​d​x​h​z
+     */
     public function domain()
     {
         return $this->belongsTo(Domain::class, 'did', 'did');
     }
 
+    /**
+     * 用户关联
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @author ikd_xhz
+     */
     public function user()
     {
         return $this->belongsTo(User::class, 'uid', 'uid');
