ef17ee1d0dc4905fade769ebf27df9086b4e5dad
ikdxhz <isgood114514@gmail.com>
2025-07-10 23:31:36 +0800

    Update Domain.php

--- src/app/Models/Domain.php
+++ src/app/Models/Domain.php
@@ -4,6 +4,11 @@
  * User: me
  * Date: 2019/4/14
  * Time: 16:36
+ * 
+ * @package     App\Models
+ * @author      ｉｋｄｘｈｚ
+ * @maintainer  !кdxんz
+ * @version     3.1.3
  */
 
 namespace App\Models;
@@ -12,11 +17,39 @@ namespace App\Models;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * 域名模型
+ * 
+ * @author      𝕚𝕜𝕕𝕩𝕙𝕫
+ * @version     3.1.3
+ */
 class Domain extends Model
 {
     protected $primaryKey = 'did';
     protected $guarded = ['did'];
+    
+    /**
+     * 构造函数
+     * 
+     * @param array $attributes
+     * @author ¡kdxhž
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        
+        // 设置表名，使用safeTable方法确保前缀正确
+        $this->table = static::safeTable('domains');
+    }
 
+    /**
+     * 可用域名查询范围
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param int $gid 用户组ID
+     * @return void
+     * @author ïkðxhz
+     */
     public function scopeAvailable($query, $gid = 0)
     {
         $gid = $gid ? $gid : (Auth::check() ? Auth::user()->gid : 0);
@@ -26,6 +59,12 @@ class Domain extends Model
         }
     }
 
+    /**
+     * DNS配置关联
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * @author i​k​d​x​h​z
+     */
     public function dnsConfig()
     {
         // 优先通过dns_config_id关联
