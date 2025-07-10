23c2390facdd712544a0b774b7d0c6fad063ff98
ikdxhz <142083737+ikdxhz@users.noreply.github.com>
2025-07-10 19:16:28 +0800

    修改域名添加和查询逻辑 使用dns_config_id替代dns字段关联
    Signed-off-by: ikdxhz <142083737+ikdxhz@users.noreply.github.com>

--- src/app/Http/Controllers/Admin/DomainController.php
+++ src/app/Http/Controllers/Admin/DomainController.php
@@ -39,12 +39,12 @@ class DomainController extends Controller
     private function domainList(Request $request)
     {
         $result = ['status' => -1];
-        $dns = $request->post('dns');
-        if (!$dns) {
-            $result['message'] = '请选择域名解析平台';
-        } elseif (!$config = DnsConfig::find($dns)) {
+        $dns_config_id = $request->post('dns_config_id');
+        if (!$dns_config_id) {
+            $result['message'] = '请选择域名解析平台配置';
+        } elseif (!$config = DnsConfig::find($dns_config_id)) {
             $result['message'] = '请先对此解析平台进行配置';
-        } elseif (!$_dns = Helper::getModel($dns)) {
+        } elseif (!$_dns = Helper::getModel($config->dns)) {
             $result['message'] = '暂不支持此域名解析平台';
         } else {
             $_dns->config($config->config);
@@ -52,7 +52,7 @@ class DomainController extends Controller
             if ($list) {
                 $data = [];
                 foreach ($list as $domain) {
-                    if (!Domain::where('domain', $domain['Domain'])->first()) {
+                    if (!Domain::where('domain', $domain['Domain'])->where('dns_config_id', $dns_config_id)->first()) {
                         $data[] = [
                             'domain' => $domain['Domain'],
                             'domain_id' => $domain['DomainId']
@@ -98,20 +98,20 @@ class DomainController extends Controller
     private function add(Request $request)
     {
         $result = ['status' => -1];
-        $dns = $request->post('dns');
+        $dns_config_id = $request->post('dns_config_id');
         $domain = $request->post('domain');
         $domain = explode(',', trim($domain));
         $desc = $request->post('desc');
         $groups = $request->post('groups');
         $point = abs(intval($request->post('point')));
 
-        if (!$dns) {
-            $result['message'] = '请选择域名解析平台';
+        if (!$dns_config_id) {
+            $result['message'] = '请选择域名解析平台配置';
         } elseif (count($domain) != 2 || !$domain[0]) {
             $result['message'] = '请选择要添加的域名';
-        } elseif (!$config = DnsConfig::find($dns)) {
+        } elseif (!$config = DnsConfig::find($dns_config_id)) {
             $result['message'] = '请先对此解析平台进行配置';
-        } elseif (!$_dns = Helper::getModel($dns)) {
+        } elseif (!$_dns = Helper::getModel($config->dns)) {
             $result['message'] = '暂不支持此域名解析平台';
         } elseif (empty($groups)) {
             $result['message'] = '请选择用户组';
@@ -122,12 +122,13 @@ class DomainController extends Controller
                 if (in_array('0', $groups)) {
                     $groups = ["0"];
                 }
-                if (Domain::where('dns', $dns)->where('domain_id', $domain[0])->first()) {
+                if (Domain::where('dns_config_id', $dns_config_id)->where('domain_id', $domain[0])->first()) {
                     $result['message'] = '此域名已经添加过';
                 } elseif (Domain::create([
                     'domain' => $domain[1],
                     'domain_id' => $domain[0],
-                    'dns' => $dns,
+                    'dns' => $config->dns,
+                    'dns_config_id' => $dns_config_id,
                     'groups' => implode(',', $groups),
                     'desc' => $desc,
                     'point' => $point
@@ -163,5 +164,4 @@ class DomainController extends Controller
         }
         return $result;
     }
-
 }
